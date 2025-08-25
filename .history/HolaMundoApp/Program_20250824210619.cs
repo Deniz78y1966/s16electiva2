3a99c867bcd65e554388ce2df771ff2f7709dbf6
Génesis Denisse Matos Rosario <genesisdenissematosrosario@NULL.Home>
2025-08-24 21:07:16 -0400

    idk anymore

---
+++ .history/HolaMundoApp/Program_20250824210619.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () =>
+{
+    var holaMundo = new HolaMundo();
+    return holaMundo.GetMessage();
+});
+
+//execute on server
+app.Run();
+
